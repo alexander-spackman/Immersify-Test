4f0f83b7f5465f34644fec532da73e9e772ed953
alexander-spackman <alexanders124@hotmail.com>
2021-08-17 10:11:35 +0100

    Part Position Standardisation
    All transform values under a reasonable margin were rounded down to zero to reduce complexity, part class started

---
+++ Imm Test/Assets/Scripts/ExpandOrderHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandOrderHandler : MonoBehaviour
+{
+    public List<GameObject> Group1 = new List<GameObject>();
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ Imm Test/Assets/Scripts/Part.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part : MonoBehaviour
+{
+
+    public enum Orientation { X, MinusX, Y, MinusY, Z, MinusZ }
+
+    public Orientation orientation;
+    public Transform OriginalPos;
+    public Transform ExpandedPos;
+     string Name;
+    void Start()
+    {
+        OriginalPos = this.transform;
+       // ExpandedPos = this.transform;
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
